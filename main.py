29aab0da4f8a2a28d232da4a221e019736a438e3
Dhaggstrom <david-hpf@hotmail.com>
2025-09-27 20:36:48 +0200

    add modularization

--- main.py
+++ main.py
@@ -2,23 +2,21 @@
 import argparse
 
 def read_csv(path):
+
     file = open(path)
     content = file.read()
     file.close()
 
     return content
-    
 
-def main():
-    parser = argparse.ArgumentParser(description="Mini CLI")
-    parser.add_argument("input", help="input path of file")
-    parser.add_argument("output", help="output path of file")
-    args = parser.parse_args()
+def write_json(path, text):
 
+    with open(path, "w") as f:
+        f.write(text)
 
-    lines = read_csv(args.input).splitlines()
+def csv_to_json(lines):
+
     header = lines[0].split(",")
-
     lines.pop(0)
 
     json = "["
@@ -43,11 +41,21 @@ def main():
 
     json = json[:-1]
     json = json + "]"
+    
+    return json
 
-    with open(args.output, "w") as f:
-        f.write(json)
+def main():
 
+    parser = argparse.ArgumentParser(description="Mini CLI")
+    parser.add_argument("input", help="input path of file")
+    parser.add_argument("output", help="output path of file")
+    args = parser.parse_args()
 
+    lines = read_csv(args.input).splitlines()
+
+    json = csv_to_json(lines)
+
+    write_json(args.output, json)
 
 
 if __name__ == "__main__":
