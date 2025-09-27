8d3bb05cad6059aa1f2b133d7bff720691a833dc
Dhaggstrom <david-hpf@hotmail.com>
2025-09-27 16:31:50 +0200

    select specific metrics

--- scripts/bench.sh
+++ scripts/bench.sh
@@ -4,4 +4,5 @@ mkdir -p benchmarks
 
 #myfilesize=$(wc -c "../data/$1" | awk '{print $1}')
 
-/usr/bin/time -v .././main.py "../data/$1" "../data/output.json" 2> benchmarks/results.txt
+#/usr/bin/time -v .././main.py "../data/$1" "../data/output.json" 2> benchmarks/results.txt
+/usr/bin/time -f "Elapsed_time: %e seconds CPU_kernel_time: %S seconds CPU_utilization: %P Max_RAM: %M Kb Exit_status: %x" .././main.py "../data/$1" "../data/output.json"
