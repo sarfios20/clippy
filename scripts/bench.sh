a91564c8e7dc7fa3b8c4dcb60a4219ceb3cd9cf7
Dhaggstrom <david-hpf@hotmail.com>
2025-09-27 17:10:33 +0200

    add filesize to metrics output

--- scripts/bench.sh
+++ scripts/bench.sh
@@ -2,9 +2,15 @@
 
 mkdir -p benchmarks
 
-#myfilesize=$(wc -c "../data/$1" | awk '{print $1}')
+current_date_time="$(date '+%Y-%m-%d_%H-%M-%S')"
+
+file_size=$(wc -c "../data/$1" | awk '{print $1}')
+file_size_KiB="$(($file_size / 1024))"
 
 #/usr/bin/time -v .././main.py "../data/$1" "../data/output.json" 2> benchmarks/results.txt
-#metrics=$(/usr/bin/time -f "Elapsed_time: %e seconds CPU_kernel_time: %S seconds CPU_utilization: %P Max_RAM: %M Kb Exit_status: %x" .././main.py "../data/$1" "../data/output.json")
-metrics=$(/usr/bin/time -f "%e %S %P %M %K %x" .././main.py "../data/$1" "../data/output.json" 2>&1 >/dev/null)
-echo $metrics
+
+metrics=$(/usr/bin/time -f "%e %S %P %M %x" .././main.py "../data/$1" "../data/output.json" 2>&1 >/dev/null)
+
+read elapsed_time system_time cpu_utilization mem_kb exit_status <<< "$metrics"
+
+echo "Start: $current_date_time Dataset: $1 File_size: $file_size_KiB KiB Elapsed_time: $elapsed_time seconds CPU_kernel_time: $system_time seconds CPU_utilization: $cpu_utilization Max_RAM: $mem_kb Kb Exit_status: $exit_status"
