60ce52d384950f66eb72d2e9bfa723b9608297b9
Dhaggstrom <david-hpf@hotmail.com>
2025-09-27 17:24:37 +0200

    fix float division in kib_per_second

--- scripts/bench.sh
+++ scripts/bench.sh
@@ -13,6 +13,6 @@ metrics=$(/usr/bin/time -f "%e %S %P %M %x" .././main.py "../data/$1" "../data/o
 
 read elapsed_time system_time cpu_utilization mem_kb exit_status <<< "$metrics"
 
-kib_per_second=$(($file_size_KiB / $elapsed_time))
+kib_per_second=$(echo "scale=2; $file_size_KiB / $elapsed_time" | bc -l)
 
-echo "Start: $current_date_time Dataset: $1 File_size: $file_size_KiB KiB Elapsed_time: $elapsed_time Kib/s: $kib_per_second seconds CPU_kernel_time: $system_time seconds CPU_utilization: $cpu_utilization Max_RAM: $mem_kb Kb Exit_status: $exit_status"
+echo "Start: $current_date_time Dataset: $1 File_size: $file_size_KiB KiB Elapsed_time: $elapsed_time Kib/s: $kib_per_second Kib/s CPU_kernel_time: $system_time seconds CPU_utilization: $cpu_utilization Max_RAM: $mem_kb Kb Exit_status: $exit_status"
