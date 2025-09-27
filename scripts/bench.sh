45ee1e51d90130aa9b90b1659a29732d85f8bcc6
Dhaggstrom <david-hpf@hotmail.com>
2025-09-27 17:40:04 +0200

    add number of lines and lines per second into metrics

--- scripts/bench.sh
+++ scripts/bench.sh
@@ -4,7 +4,11 @@ mkdir -p benchmarks
 
 current_date_time="$(date '+%Y-%m-%d_%H-%M-%S')"
 
-file_size=$(wc -c "../data/$1" | awk '{print $1}')
+
+file_data=$(wc -lc "../data/$1" | awk '{print $1,$2}')
+
+read number_lines file_size <<< "$file_data"
+
 file_size_KiB="$(($file_size / 1024))"
 
 #/usr/bin/time -v .././main.py "../data/$1" "../data/output.json" 2> benchmarks/results.txt
@@ -14,5 +18,6 @@ metrics=$(/usr/bin/time -f "%e %S %P %M %x" .././main.py "../data/$1" "../data/o
 read elapsed_time system_time cpu_utilization mem_kb exit_status <<< "$metrics"
 
 kib_per_second=$(echo "scale=2; $file_size_KiB / $elapsed_time" | bc -l)
+lines_per_second=$(echo "scale=2; $number_lines / $elapsed_time" | bc -l)
 
-echo "Start: $current_date_time Dataset: $1 File_size: $file_size_KiB KiB Elapsed_time: $elapsed_time Kib/s: $kib_per_second Kib/s CPU_kernel_time: $system_time seconds CPU_utilization: $cpu_utilization Max_RAM: $mem_kb Kb Exit_status: $exit_status"
+echo "Start: $current_date_time Dataset: $1 File_size: $file_size_KiB KiB Elapsed_time: $elapsed_time seconds Kib/s: $kib_per_second Kib/s Number_lines: $number_lines Lines_per_second: $lines_per_second CPU_kernel_time: $system_time seconds CPU_utilization: $cpu_utilization Max_RAM: $mem_kb Kb Exit_status: $exit_status"
