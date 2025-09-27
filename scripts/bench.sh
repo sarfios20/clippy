beb8da79a9f752aea433cdf456665e86369e5a96
Dhaggstrom <david-hpf@hotmail.com>
2025-09-27 15:46:23 +0200

    add argument input to bechmark script

--- scripts/bench.sh
+++ scripts/bench.sh
@@ -2,6 +2,6 @@
 
 mkdir -p benchmarks
 
-dataset="test.csv"
+#myfilesize=$(wc -c "../data/$1" | awk '{print $1}')
 
-/usr/bin/time -v .././main.py "../data/$dataset" "../data/output.json" 2> benchmarks/results.txt
+/usr/bin/time -v .././main.py "../data/$1" "../data/output.json" 2> benchmarks/results.txt
